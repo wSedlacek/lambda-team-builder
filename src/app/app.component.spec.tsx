f24eab043caf4caa28a27b57cdd2da3a4e964f95
William Sedlacek <wsedlacekc@gmail.com>
2019-09-18 13:05:01 -0500

    Put it all together

--- src/app/app.component.spec.tsx
+++ src/app/app.component.spec.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+
+import { render, act } from '@testing-library/react';
 
 import { App } from './app.component';
+import { TeamService } from './services/team.service';
 
 it('renders without crashing', () => {
   const { baseElement } = render(<App />);
   expect(baseElement).toBeInTheDocument();
 });
 
-it('has items', () => {
+it('can display items', () => {
   const { baseElement } = render(<App />);
+
+  act(() => {
+    TeamService.addMember({ id: '', name: 'Dave' });
+  });
+
   expect(baseElement.querySelector('.Item')).toBeInTheDocument();
 });
--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import './app.component.scss';
 
-import { TeamService } from './services/team.service';
+import { Add } from './components/add/add.component';
 import { Item } from './components/item/item.component';
+import { TeamService } from './services/team.service';
 
 import { TeamMember } from './models/TeamMember';
 
 export const App = () => {
   const [members, setMembers] = React.useState<TeamMember[]>([]);
 
-  React.useEffect(() => setMembers(TeamService.getMembers()), []);
+  React.useEffect(() => {
+    const subscription = TeamService.subscribe(setMembers);
+    return () => subscription.unsubscribe();
+  }, []);
 
   return (
     <div>
+      <Add handleSubmit={member => TeamService.addMember(member)} />
       {members.map(member => (
         <Item member={member} key={member.id} />
       ))}
